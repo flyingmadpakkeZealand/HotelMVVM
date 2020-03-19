fad3e21de8e728ae80a4fd230bf5a2f59a58948e
Magnus Bahn Berger <magn351i@edu.easj.dk>
2020-03-19 12:43:15 +0100

    Added UnitTest. Changed CatalogSingleton.

--- HotelMVVM/Model/CatalogSingleton.cs
+++ HotelMVVM/Model/CatalogSingleton.cs
@@ -11,35 +11,92 @@ namespace HotelMVVM.Model
 {
     public class CatalogSingleton<T>
     {
+        #region CatalogGetterOld
+        //private static Dictionary<Type, object> _singletons = new Dictionary<Type, object>();
+
+        //public static CatalogSingleton<T> GetCatalog()
+        //{
+        //    if (!_singletons.ContainsKey(typeof(T)))
+        //    {
+        //        var catalog = new CatalogSingleton<T>();
+        //        _singletons.Add(typeof(T), catalog);
+        //        catalog._preLoaded = true;
+        //    }
+
+        //    return (CatalogSingleton<T>)_singletons[typeof(T)];
+        //}
+
+        //private static Dictionary<Type, object> _pendingKeys = new Dictionary<Type, object>();
+        //public static CatalogSingleton<T> GetCatalog(Action notify, Consumer<T> consumer) //This should maybe be changed to be more like the observable collection itself, implementing a subscription based system, maybe with an event. 
+        //{ //Or a last come, first served principle could be adopted.
+        //    if (!_singletons.ContainsKey(typeof(T)))
+        //    {
+        //        if (_pendingKeys.ContainsKey(typeof(T)))
+        //        {
+        //            Loop();
+        //            return (CatalogSingleton<T>)_pendingKeys[typeof(T)];
+        //            async void Loop()
+        //            {
+        //                await Task.Run(() =>
+        //                {
+        //                    while (_pendingKeys.ContainsKey(typeof(T)))
+        //                    {
+        //                        Thread.Sleep(100);
+        //                    }
+        //                });
+        //                notify();
+        //            }
+        //        }
+        //        CatalogSingleton<T> catalog = new CatalogSingleton<T>();
+        //        _pendingKeys.Add(typeof(T), catalog);
+        //        LoadItems();
+        //        return catalog;
+
+        //        async void LoadItems()
+        //        {
+        //            List<T> items = await consumer.GetAsync();
+        //            catalog.RefillCatalog(items);
+        //            _singletons.Add(typeof(T), catalog);
+        //            _pendingKeys.Remove(typeof(T));
+        //            notify();
+        //            catalog._preLoaded = true;
+        //        }
+        //    }
+
+        //    notify();
+        //    return (CatalogSingleton<T>)_singletons[typeof(T)];
+        //}
+
+        //private void RefillCatalog(IEnumerable<T> collection)
+        //{
+        //    _catalog = new ObservableCollection<T>(collection);
+        //}
+
+        //private bool _preLoaded;
+
+        //public bool PreLoaded
+        //{
+        //    get { return _preLoaded; }
+        //}
+        #endregion //New info about static fields in generics means that all of this completely outdated and should be redone.
+
         #region CatalogGetter
-        private static Dictionary<Type, object> _singletons = new Dictionary<Type, object>();
-
-        public static CatalogSingleton<T> GetCatalog()
+        private static CatalogSingleton<T> _pendingCatalog;
+        private static CatalogSingleton<T> _singleton;
+        public static CatalogSingleton<T> GetCatalog(Action notify, Consumer<T> consumer)
         {
-            if (!_singletons.ContainsKey(typeof(T)))
+            if (_singleton == null)
             {
-                var catalog = new CatalogSingleton<T>();
-                _singletons.Add(typeof(T), catalog);
-                catalog._preLoaded = true;
-            }
-
-            return (CatalogSingleton<T>)_singletons[typeof(T)];
-        }
-
-        private static Dictionary<Type, object> _pendingKeys = new Dictionary<Type, object>();
-        public static CatalogSingleton<T> GetCatalog(Action notify, Consumer<T> consumer) //This should maybe be changed to be more like the observable collection itself, implementing a subscription based system, maybe with an event. 
-        { //Or a last come, first served principle could be adopted.
-            if (!_singletons.ContainsKey(typeof(T)))
-            {
-                if (_pendingKeys.ContainsKey(typeof(T)))
+                if (_pendingCatalog != null)
                 {
                     Loop();
-                    return (CatalogSingleton<T>)_pendingKeys[typeof(T)];
+                    return _pendingCatalog;
+
                     async void Loop()
                     {
                         await Task.Run(() =>
                         {
-                            while (_pendingKeys.ContainsKey(typeof(T)))
+                            while (_pendingCatalog != null)
                             {
                                 Thread.Sleep(100);
                             }
@@ -47,8 +104,9 @@ namespace HotelMVVM.Model
                         notify();
                     }
                 }
+
                 CatalogSingleton<T> catalog = new CatalogSingleton<T>();
-                _pendingKeys.Add(typeof(T), catalog);
+                _pendingCatalog = catalog;
                 LoadItems();
                 return catalog;
 
@@ -56,16 +114,17 @@ namespace HotelMVVM.Model
                 {
                     List<T> items = await consumer.GetAsync();
                     catalog.RefillCatalog(items);
-                    _singletons.Add(typeof(T), catalog);
-                    _pendingKeys.Remove(typeof(T));
+                    _singleton = catalog;
+                    _pendingCatalog = null;
                     notify();
                     catalog._preLoaded = true;
                 }
             }
 
             notify();
-            return (CatalogSingleton<T>)_singletons[typeof(T)];
+            return _singleton;
         }
+
 
         private void RefillCatalog(IEnumerable<T> collection)
         {
@@ -77,8 +136,8 @@ namespace HotelMVVM.Model
         public bool PreLoaded
         {
             get { return _preLoaded; }
-        }
-        #endregion //New info about static fields in generics means that all of this completely outdated and should be redone.
+        } 
+        #endregion
 
         public ObservableCollection<T> Catalog
         {
